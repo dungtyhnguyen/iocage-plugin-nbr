4b4d6460dd4c4dacaffb6e63459c5e0e99fab316
dungtyhnguyen <88713322+dungtyhnguyen@users.noreply.github.com>
2021-11-02 16:23:47 +0700

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 PRODUCT='NAKIVO Backup & Replication'
-URL="http://192.168.1.24:280/10.3/NBRv10.3.0.58591.sh"
-SHA256="6c6a7a1f4f229d58e3959f433153f4bfc56cde08835c80a63e3a613ec85c4414"
+URL="http://192.168.1.24:280/10.5/NAKIVO10.5.0.59827.sh"
+SHA256="7618cbad2043b2b4720a83f80cd88c4ebf2baf4a1c500db4930559d6eea61453"
 
 PRODUCT_ROOT="/usr/local/nakivo"
 INSTALL="inst.sh"
